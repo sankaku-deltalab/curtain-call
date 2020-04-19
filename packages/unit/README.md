935a344d769be8f0033dc7ea8603f0665401dbba
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-04-19 15:37:45 +0900

    Add util repo

---
+++ packages/unit/README.md
@@ -0,0 +1,11 @@
+# `@curtain-call/util`
+
+> TODO: description
+
+## Usage
+
+```
+import util from '@curtain-call/util';
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/unit/__tests__/transformation.test.ts
@@ -0,0 +1,7 @@
+import { Transformation } from "../src";
+
+describe("@curtain-call/util.Transformation", () => {
+  it("needs tests", () => {
+    expect(Transformation).not.toBeUndefined();
+  });
+});
