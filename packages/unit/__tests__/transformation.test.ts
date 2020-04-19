d25056a3bf07d031d0364e580e09e670ff07d45f
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-04-19 22:39:47 +0900

    Add Transformation

--- packages/unit/__tests__/transformation.test.ts
+++ packages/unit/__tests__/transformation.test.ts
@@ -1,7 +1,73 @@
 import { Transformation } from "../src";
+import { Matrix } from "trans-vector2d";
 
 describe("@curtain-call/util.Transformation", () => {
-  it("needs tests", () => {
-    expect(Transformation).not.toBeUndefined();
+  it("has local matrix", () => {
+    const trans = new Transformation();
+    expect(trans.getLocal()).toBeInstanceOf(Matrix);
+  });
+
+  it("has global matrix", () => {
+    const trans = new Transformation();
+    expect(trans.getGlobal()).toBeInstanceOf(Matrix);
+  });
+
+  describe("can attach to parent Transformation", () => {
+    it("from function", () => {
+      const parent = new Transformation();
+      const child = new Transformation();
+      expect(() => child.attachTo(parent)).not.toThrowError();
+    });
+
+    it("and can detach", () => {
+      const parent = new Transformation();
+      const child = new Transformation();
+      child.attachTo(parent);
+
+      expect(() => child.detachFromParent()).not.toThrowError();
+    });
+
+    it("and update global Matrix of self when attached", () => {
+      const parent = new Transformation().setLocal(
+        Matrix.identity.translate({ x: 1, y: 2 })
+      );
+      const child = new Transformation().setLocal(
+        Matrix.identity.translate({ x: 3, y: 4 })
+      );
+
+      child.attachTo(parent);
+
+      const expectedGlobal = Matrix.identity.translate({ x: 4, y: 6 });
+      expect(child.getGlobal()).toEqual(expectedGlobal);
+    });
+
+    it("and update global Matrix of self when parent local was changed", () => {
+      const parent = new Transformation();
+      const child = new Transformation().setLocal(
+        Matrix.identity.translate({ x: 3, y: 4 })
+      );
+
+      parent.setLocal(Matrix.identity.translate({ x: 1, y: 2 }));
+      child.attachTo(parent);
+
+      const expectedGlobal = Matrix.identity.translate({ x: 4, y: 6 });
+      expect(child.getGlobal()).toEqual(expectedGlobal);
+    });
+
+    it("and update global Matrix of self when parent attached to other", () => {
+      const parent = new Transformation();
+      const child = new Transformation().setLocal(
+        Matrix.identity.translate({ x: 3, y: 4 })
+      );
+
+      child.attachTo(parent);
+      const parentOfParent = new Transformation().setLocal(
+        Matrix.identity.translate({ x: 1, y: 2 })
+      );
+      parent.attachTo(parentOfParent);
+
+      const expectedGlobal = Matrix.identity.translate({ x: 4, y: 6 });
+      expect(child.getGlobal()).toEqual(expectedGlobal);
+    });
   });
 });
--- packages/unit/src/transformation.ts
+++ packages/unit/src/transformation.ts
@@ -1,1 +1,45 @@
-export class Transformation {}
+import { Matrix } from "trans-vector2d";
+
+export class Transformation {
+  private local = Matrix.identity;
+  private global = Matrix.identity;
+  private parent?: Transformation;
+  private readonly children = new Set<Transformation>();
+
+  setLocal(matrix: Matrix): this {
+    this.local = matrix;
+    const parentGlobal = this.parent?.global || Matrix.identity;
+    this.updateGlobal(parentGlobal);
+    return this;
+  }
+
+  getLocal(): Matrix {
+    return this.local;
+  }
+
+  getGlobal(): Matrix {
+    return this.global;
+  }
+
+  attachTo(parent: Transformation): this {
+    this.parent?.children.delete(this);
+    this.parent = parent;
+    parent.children.add(this);
+    this.updateGlobal(parent.global);
+    return this;
+  }
+
+  detachFromParent(): this {
+    if (!this.parent) return this;
+
+    this.parent.children.delete(this);
+    this.parent = undefined;
+    this.updateGlobal(Matrix.identity);
+    return this;
+  }
+
+  private updateGlobal(parentGlobalMatrix: Matrix): void {
+    this.global = parentGlobalMatrix.globalize(this.local);
+    this.children.forEach((child) => child.updateGlobal(this.global));
+  }
+}
