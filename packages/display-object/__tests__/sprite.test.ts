f85c345297dd272a9e9a5c6a3a22e4a62e9e7e4b
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-04-20 22:24:09 +0900

    Implement Sprite

--- packages/display-object/__tests__/sprite.test.ts
+++ packages/display-object/__tests__/sprite.test.ts
@@ -1,7 +1,46 @@
-// import { Sprite } from "../src";
+import * as PIXI from "pixi.js";
+import { Transformation } from "@curtain-call/util";
+import { Sprite } from "../src";
+import { Matrix, Vector } from "@curtain-call/util/node_modules/trans-vector2d";
 
 describe("@curtain-call/display-object.Sprite", () => {
-  it("needs tests", () => {
-    expect(true).toBe(true);
+  it("has Transformation", () => {
+    const sprite = new Sprite();
+    expect(sprite.trans).toBeInstanceOf(Transformation);
+  });
+
+  it("update sprite transform by Transformation", () => {
+    const pixiSprite = new PIXI.Sprite();
+    const sprite = new Sprite(pixiSprite);
+
+    sprite.trans.setLocal(Matrix.identity.translate({ x: 1, y: 2 }));
+    sprite.update();
+
+    expect(pixiSprite.position.x).toBeCloseTo(1);
+    expect(pixiSprite.position.y).toBeCloseTo(2);
+  });
+
+  it("can set texture", () => {
+    const pixiSprite = new PIXI.Sprite();
+    const sprite = new Sprite(pixiSprite);
+
+    const texture = new PIXI.Texture(new PIXI.BaseTexture());
+    sprite.setTexture(texture);
+
+    expect(pixiSprite.texture).toBe(texture);
+  });
+
+  it("can set size", () => {
+    const pixiSprite = new PIXI.Sprite();
+    const sprite = new Sprite(pixiSprite);
+
+    const texture = new PIXI.Texture(new PIXI.BaseTexture());
+    jest.spyOn(texture, "width", "get").mockReturnValue(3);
+    jest.spyOn(texture, "height", "get").mockReturnValue(4);
+    sprite.setTexture(texture);
+    sprite.setSize(new Vector(1, 2));
+
+    expect(pixiSprite.scale.x).toBeCloseTo(1 / 3);
+    expect(pixiSprite.scale.y).toBeCloseTo(2 / 4);
   });
 });
--- packages/display-object/src/sprite.ts
+++ packages/display-object/src/sprite.ts
@@ -1,1 +1,67 @@
-export class Sprite {}
+import * as PIXI from "pixi.js";
+import { Vector } from "trans-vector2d";
+import { Transformation, matrix2dToPixiMatrix } from "@curtain-call/util";
+
+/**
+ * Sprite.
+ *
+ * @example
+ * import { Vector, Matrix } from "trans-vector2d";
+ *
+ * const sprite = new Sprite()
+ *   .setTexture(foo)
+ *   .setSize(new Vector(1, 2));
+ *
+ * sprite.trans.setLocal(Matrix.from({ translation: {x: 1, y: 2} }));
+ * sprite.update();
+ */
+export class Sprite {
+  /** Transformation of self. */
+  public readonly trans = new Transformation();
+  private size = Vector.one;
+
+  /**
+   * @param pixiSprite Pixi sprite.
+   */
+  constructor(public readonly pixiSprite = new PIXI.Sprite()) {
+    pixiSprite.anchor = new PIXI.Point(0.5, 0.5);
+  }
+
+  /**
+   * Update pixi sprite transformation.
+   */
+  update(): void {
+    this.pixiSprite.transform.setFromMatrix(
+      matrix2dToPixiMatrix(this.trans.getGlobal())
+    );
+  }
+
+  /**
+   * Set texture.
+   *
+   * @param texture New texture.
+   * @returns this.
+   */
+  setTexture(texture: PIXI.Texture): this {
+    this.pixiSprite.texture = texture;
+    this.updateSpriteSize();
+    return this;
+  }
+
+  /**
+   * Set sprite size.
+   *
+   * @param size New size.
+   * @returns this.
+   */
+  setSize(size: Vector): this {
+    this.size = size;
+    this.updateSpriteSize();
+    return this;
+  }
+
+  private updateSpriteSize(): void {
+    this.pixiSprite.scale.x = this.size.x / this.pixiSprite.texture.width;
+    this.pixiSprite.scale.y = this.size.y / this.pixiSprite.texture.height;
+  }
+}
