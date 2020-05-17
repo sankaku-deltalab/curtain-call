4fe4f78b3c361bc1b15f2ec124cf025320790a07
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-05-17 20:50:35 +0900

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ["jest-canvas-mock"],
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
   collectCoverageFrom: ["packages/**/src/**/*.ts"]
 };
