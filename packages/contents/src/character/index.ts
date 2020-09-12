9ba45631564e0438c45b046487cd94343beb18a3
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-09-12 21:53:16 +0900

    Delete contents

--- packages/contents/src/character/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./character";
-export * from "./null-plan";
-export * from "./plan";
-export * from "./simple-enemy-plan";
--- packages/contents/src/weapon/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./nearest-character-provider";
-export * from "./simple-bullet";
-export * from "./simple-bullet-generator";
