6e8028e87e54b4802766ed549d52897a5dfbc77b
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-09-20 19:04:07 +0900

    Add ActorWithInfo as actor subsystem

--- packages/actor/src/actor-subsystem/index.ts
+++ packages/actor/src/actor-subsystem/index.ts
@@ -2,6 +2,7 @@ export * from "./actor-with-collision";
 export * from "./actor-with-controller";
 export * from "./actor-with-damaging";
 export * from "./actor-with-display-object";
+export * from "./actor-with-info";
 export * from "./actor-with-sub-actors";
 export * from "./actor-with-transformation";
 export * from "./actor-with-world";
