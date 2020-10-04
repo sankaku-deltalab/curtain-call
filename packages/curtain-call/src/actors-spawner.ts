5e3623afadfdbd8e3848ddaf40563ff2f8c19619
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-10-04 23:09:07 +0900

    Fix exports in curtain-call

--- packages/curtain-call/src/actors-spawner.ts
+++ packages/curtain-call/src/actors-spawner.ts
@@ -1,11 +1,15 @@
 import { EventEmitter } from "eventemitter3";
 import { Transformation, FiniteResource } from "@curtain-call/util";
 import { Collision } from "@curtain-call/collision";
-import { ActorsSpawner, diContainer } from "@curtain-call/actors-spawner";
+import {
+  ActorsSpawner,
+  ActorsSpawnerManager,
+  diContainer,
+} from "@curtain-call/actors-spawner";
 
 diContainer.register("EventEmitter", EventEmitter);
 diContainer.register("Transformation", Transformation);
 diContainer.register("FiniteResource", FiniteResource);
 diContainer.register("Collision", Collision);
 
-export { ActorsSpawner };
+export { ActorsSpawner, ActorsSpawnerManager };
