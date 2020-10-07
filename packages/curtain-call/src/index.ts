a1a83efec6d49ceedf4498caa85d76e56b9d65de
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-10-07 20:59:31 +0900

    Fix injection

--- packages/curtain-call/src/index.ts
+++ packages/curtain-call/src/index.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 
 export * from "@curtain-call/asset";
 export * from "@curtain-call/level";
-export * from "@curtain-call/mover";
 export * from "@curtain-call/timer";
 
 export * from "./actor";
@@ -12,6 +11,7 @@ export * from "./collision";
 export * from "./display-object";
 export * from "./engine";
 export * from "./input";
+export * from "./mover";
 export * from "./util";
 export * from "./weapon";
 export * from "./world";
