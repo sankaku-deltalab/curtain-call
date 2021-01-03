f7c235e660d9e8974067166724e75ac44177dc79
Sankaku <sankaku_dlt.45631@outlook.jp>
2021-01-03 19:23:28 +0900

    Fix entity exporting

--- packages/entity/src/index.ts
+++ packages/entity/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./actor-base";
 export * from "./actor-extension";
 export * from "./interfaces";
+export * from "./time-schedule-base";
 export * from "./world-base";
 export * from "./world-extension";
