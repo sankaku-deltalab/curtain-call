c170f8dfd7df464a03c61626ddd89c95eae1a1d0
Sankaku <sankaku_dlt.45631@outlook.jp>
2021-01-30 20:13:23 +0900

    Add uc-transformation package

---
+++ packages/uc-transformation/README.md
@@ -0,0 +1,11 @@
+# `@curtain-call/uc-transformation`
+
+> TODO: description
+
+## Usage
+
+```
+import ucTransformation from '@curtain-call/uc-transformation';
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/uc-transformation/src/entity-impl/actors-mover-impl.ts
@@ -0,0 +1,7 @@
+import { injectable } from "@curtain-call/shared-dependencies";
+// import { ActorsMover } from "@curtain-call/entity";
+
+@injectable()
+export class ActorsMoverImpl {
+  // TODO: impl
+}
